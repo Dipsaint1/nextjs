c8c7a44b74ead749e3c8a5447edf20de3f163581
Cyber9ja <ayodejioladapo96@gmail.com>
2024-01-06 22:10:24 +0000

    minor update

--- app/page.js
+++ app/page.js
@@ -3,10 +3,11 @@ import Link from 'next/link';
 const Home = () => {
   return (
     <div>
-      <h1 className="text-7xl">Homepage</h1>
-      <Link href='/about'>about</Link>
+      <h1 className='text-5xl mb-8 font-bold'>HomePage</h1>
+      <Link href='/client' className='btn btn-accent'>
+        get started
+      </Link>
     </div>
-  )
-}
-
-export default Home
+  );
+};
+export default Home;
