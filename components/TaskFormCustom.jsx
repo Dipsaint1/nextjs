0d9c537217f4bb79bdfb2d45abc5eb3618589750
Cyber9ja <ayodejioladapo96@gmail.com>
2024-01-17 22:47:50 +0000

    toast

--- components/TaskFormCustom.jsx
+++ components/TaskFormCustom.jsx
@@ -1,6 +1,8 @@
 "use client"
 import { useFormStatus, useFormState } from "react-dom";
 import { createTaskCustom } from "@/utils/actions";
+import { useEffect } from "react";
+import toast from 'react-hot-toast';
 
 const SubmitBtn = () => {
   const { pending } = useFormStatus();
@@ -18,9 +20,32 @@ const initialState = { message: null };
 const TaskFormCustom = () => {
   const [ formState, formAction ] = useFormState(createTaskCustom, initialState);
 
+  // useEffect(() => {
+  //   if(formState?.message) {
+  //     setTimeout(() => {
+  //       formAction.reset();
+  //     }, 3000);
+  //   }
+  //   return () => {
+  //     formAction.reset();
+  //   } 
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+
+  // }, []);
+  
+  useEffect(() => {
+    if(formState?.message === 'error' ){
+      toast.error('There was an error!');
+      return;
+    } if(formState.message === 'success'){
+      toast.success('Task created!');
+    }
+  }, [formState]);
+  
+
   return (
     <form action={formAction}>
-      { formState?.message ? <p className="mb-2">{formState?.message}</p> : null }
+      {/* { formState?.message ? <p className="mb-2">{formState?.message}</p> : null } */}
       <div className='join w-full'>
         <input type='text' className='input input-bordered join-item w-full' placeholder='type here' name='content' required/>
         <SubmitBtn/>
